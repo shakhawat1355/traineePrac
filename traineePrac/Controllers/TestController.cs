b098cc38fbd35d36e9aadabcd52306cef88bae60
shakhawat1355 <hossain.shakhawat@brainstation-23.com>
2023-05-08 13:11:15 +0600

    Add project files.

---
+++ traineePrac/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace traineePrac.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Example()
+        {
+            return View();
+        }
+        public IActionResult ViewBagExample()
+        {
+            ViewBag.Contents = "this is a content inside a viewbag";
+            return View();
+        }
+        public IActionResult ViewBagExampleWithList()
+        {
+            List<string> names = new List<string> { "John", "Alice", "Bob" };
+            ViewBag.NamesList = names;
+            return View();
+        
+        }
+
+        public IActionResult ViewBagExampleWithObject()
+        {
+            var obj = new { name=" abc", age = 40 };
+            ViewBag.obj = obj;
+            return View();
+
+        }
+    }
+}
---
+++ traineePrac/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace traineePrac.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
